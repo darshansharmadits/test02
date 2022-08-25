8b897fbfcd2f85651bf20ae41886b3afe0f399b2
Darshan <darshan@Darshans-MacBook-Air.local>
2022-08-25 18:25:58 +0530

    added unit test cases for user component

--- src/tests/user/user.test.tsx
+++ src/tests/user/user.test.tsx
@@ -1,5 +1,6 @@
-import { convertMetauserDataToUserData } from "../../user/converters/converter";
-
+import usersDataJson from "../../user/metadata/users.json";
+import userLogsJson from "../../user/metadata/logs.json";
+import Card from "../../user/ui/card";
 
 const mockGetUserDataFunction = jest.fn().mockImplementation(() => {
     return {
@@ -20,9 +21,31 @@ const mockGetUserDataFunction = jest.fn().mockImplementation(() => {
         revenue: 0,
         time: "test-time"
     }
-}}
-);
+}});
 
+const mockGetUserConversions = jest.fn().mockImplementation(() => {
+    return {
+        1: {
+        "2013-04-01": {
+            impressions: 0,
+            conversions: 0,
+        },
+        "2013-04-02": {
+            impressions: 0,
+            conversions: 1,
+        },
+    },2: {
+        "2013-04-01": {
+            impressions: 0,
+            conversions: 0,
+        },
+        "2013-04-02": {
+            impressions: 0,
+            conversions: 1,
+        }
+    } 
+}}
+).mockName("getUserConversionsData");
 
 
 const userTest = describe('Testing user component', () => {
@@ -31,16 +54,75 @@ const userTest = describe('Testing user component', () => {
     });
 
     it("checking if user data is valid", () => {
-        expect(mockGetUserDataFunction[1]).toBeDefined();
+        expect(mockGetUserDataFunction.mock.results[1]).toBeDefined;
     });
-    it("Check if user has a valid id", () => {
-        expect(mockGetUserDataFunction[0]).toBeUndefined();
+    it("testing invalid user ids", () => {
+        expect(mockGetUserDataFunction[0]).toBeUndefined;
     });
-
-    // it("Check if user has a valid id", () => {
-    //     const resp = mockGetUserDataFunction();
-    // });
-
+    it("user conversion data validity test", () => {
+        expect(Object.keys(mockGetUserConversions).length).toBeGreaterThan(0);
+    });
+    it("user conversion for invalid user id", () => {
+        expect(mockGetUserConversions.mock.results[0]).toBeUndefined;
+    });
 });
 
-export default userTest;
+
+const jsonImportTests = describe('Testing user data from Json File', () => {
+    it("user conversion data validity test", () => {
+        expect(usersDataJson.length).toBeGreaterThan(0);
+    });
+    it("checking type of user data from json", () => {
+        expect(typeof usersDataJson).toEqual('object');
+    });
+    it("checking if their is any data present in logs json", () => {
+        expect(userLogsJson.length).toBeGreaterThan(0);
+    });
+    it("checking type of logs data from json", () => {
+        expect(typeof userLogsJson).toEqual('object');
+    });
+    
+});
+
+
+const userCardTests = describe('Testing user card component', () => {
+    const mockUser = {
+        name: "test user 01",
+        occupation: "test occupation",
+        avatar: "test-avatar",
+        impression: 0,
+        conversion: 0,
+        revenue: 0,
+        time: "test-time"
+    }
+    const mockConversionData = {
+        1: {
+            "2013-04-01": {
+                impressions: 0,
+                conversions: 0,
+            },
+            "2013-04-02": {
+                impressions: 0,
+                conversions: 1,
+            },
+        },2: {
+            "2013-04-01": {
+                impressions: 0,
+                conversions: 0,
+            },
+            "2013-04-02": {
+                impressions: 0,
+                conversions: 1,
+            }
+        }
+    };
+    const userCard = <Card user={mockUser} userConversionData={mockConversionData} />
+    it("does user card functional component exists", () => {
+        expect(typeof Card).toEqual("function");
+    });
+    it("does user card work on passing props", () => {
+        expect(Object.keys(userCard.props).length).toEqual(2);
+    });
+});
+
+export default {userTest, jsonImportTests, userCardTests};
